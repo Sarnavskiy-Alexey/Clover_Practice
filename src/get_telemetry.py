79ef2bc5c9b78fefad2f373b75082928a1f265ed
Alexey Sarnavskiy <alexey.sarnavskiy@ya.ru>
2024-11-20 22:54:42 +0300

    Added telemetry

--- src/get_telemetry.py
+++ src/get_telemetry.py
@@ -2,10 +2,21 @@
 
 import rospy
 from clover import srv
+import os
+import datetime
 
-rospy.init_node('flight')
+rospy.init_node('get_telemetry')
 
 get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
 
 # Print drone's state
-print(get_telemetry())
+
+log_counter = 0
+while os.path.exists(os.path.join(os.path.curdir, f'log_{log_counter}.log')):
+    log_counter += 1
+
+with open(f"log_{log_counter}.log", mode='wt') as f:
+    f.write(f"Start time: {datetime.datetime.now()}\n\n")
+    while True:
+        f.write(f"{str(get_telemetry())}\n\n")
+        rospy.sleep(1)
